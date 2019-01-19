3c60d1a11f96a050580b9790f192904dfdd49d37
neverm1nd <reddigitalsw@gmail.com>
2019-01-19 03:56:59 +0300

    Add 100 minimal range

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 export class counter {
   start : number;
   range : number;
 }
+
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
@@ -15,43 +16,38 @@ export class CounterComponent implements OnInit {
 
   counter : counter = {
     start : 0,
-    range: 0
+    range: 100
   };
   result : number = 0;
-  points : number = 0;
+  points : number = 100;
   dotsX: Array<number> = [0, 100, 1500, 2500, 3500, 4000, 4500, 4800, 4900, 5000, 5100, 5200, 5300, 5400, 5500];
   dotsY: Array<number> = [0, 188, 2820, 4980, 7700, 9440, 11540, 13040, 13654, 14177, 14921, 15752, 16716, 17808, 19120];
 
-  count() {
-    if (this.counter.start > 5500) this.counter.start = 5475;
-    if (this.counter.start < 0 ) this.counter.start = -this.counter.start;
-    this.points = this.counter.range - this.counter.start;
-    this.result = Math.ceil(this.approx(this.dotsX, this.dotsY, this.counter.range)) - Math.ceil(this.approx(this.dotsX, this.dotsY,this.counter.start));
+  count(start: number, range: number) {
+    if (start > 5500) start = 5475;
+    if (start < 0 ) start = Math.abs(start);
+    this.points = Math.abs(range - start);
+    this.result = Math.abs(Math.ceil(this.approx(this.dotsX, this.dotsY, range)) - Math.ceil(this.approx(this.dotsX, this.dotsY, start)));
   }
 
-  approx(X:Array<number>, Y:Array<number>, x:number) {  //аппроксимация графика
-    // вычисление ближайшего большего и его индекса
+  approx(X:Array<number>, Y:Array<number>, x:number) {
                 let max : number = X[0];
-                let indexMax : number= 0;  // индекс
+                let indexMax : number= 0;
                  for (let i = 0; i<X.length + 1; i++)
                     {
                         if (X[i] <= max && x <= X[i]) {max = X[i]; indexMax = i;}
                     }
-  // вычисление ближайшего меньшего и его индекса
                 let min: number = X.length + 1;
                 let indexMin: number = 0;
                  for (let i = 0; i<X.length + 1; i++)
                     {
                         if (X[i] >= min && x >= X[i]) {min = X[i]; indexMin = i;};
                     }
-    // вычисление нужного Y
-    // console.log(max, min, indexMin, indexMax)
-    let y:number = ((max-x) / (max-min))*Y[indexMin] +((x-min)/(max-min))*Y[indexMax];
-    // console.log(y);
-    return y;
+    let result:number = ((max-x) / (max-min))*Y[indexMin] +((x-min)/(max-min))*Y[indexMax];
+    return result;
   }
 
   ngOnInit() {
+    this.count(this.counter.range, this.counter.start);
   }
-
 }
